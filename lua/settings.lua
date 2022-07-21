79a49a6ca0ace78c41fe04cd844c1c4d820865df
calvinludwig <bludwig.calvin@gmail.com>
2022-07-20 21:27:59 -0300

    initial commit

---
+++ lua/settings.lua
@@ -0,0 +1,9 @@
+vim.g.tokyonight_style = "night"
+vim.cmd([[
+    try
+        colorscheme tokyonight
+    catch /^Vim\%((\a\+)\)\=:E185/
+        colorscheme slate
+        set background=dark
+    endtry
+]])
